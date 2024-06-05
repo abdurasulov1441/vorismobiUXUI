85dfcc5230d0c959afa31bbae681b14d19234deb
unknown <you@example.com>
2024-06-05 10:04:57 +0500

    abdurasulov

---
+++ lib/screens/userMainPageAuthed.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          Text(
+            'Demonstration',
+          ),
+        ]));
+  }
+}
