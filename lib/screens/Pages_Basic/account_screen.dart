493ff7fa0cece84da50ac58e4ee1537808fe850e
unknown <abdurasulov1024@gmail.com>
2024-07-22 15:54:06 +0500

    hacker

--- lib/screens/Pages_Basic/account_screen.dart
+++ lib/screens/Pages_Basic/account_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/components/appbar_title.dart';
 import 'package:flutter_application_1/screens/Pages_Basic/accaount_screens/shartnomalar.dart';
 import 'package:flutter_application_1/screens/Pages_Basic/accaount_screens/visacard.dart';
-import 'package:flutter_application_1/components/imzoqoyish.dart';
+
 import 'package:flutter_application_1/style/app_colors.dart';
 import 'package:flutter_application_1/style/app_style.dart';
 import 'package:flutter_application_1/services/changeColorProvider.dart';
@@ -79,7 +79,7 @@ class _AccountScreenState extends State<AccountScreen> {
     ];
     final List<Widget> route = [
       Shartnomalar(),
-      Imzoqoyish(),
+      Shartnomalar(),
       Shartnomalar(),
       Cards(),
       Shartnomalar(),
--- lib/screens/Pages_Basic/tarifs_screens/object/ariza_texnik_object.dart
+++ lib/screens/Pages_Basic/tarifs_screens/object/ariza_texnik_object.dart
@@ -1,28 +1,33 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/components/mini_red_app_bar.dart';
+import 'package:flutter_application_1/screens/Pages_Basic/home_screen.dart';
+import 'package:flutter_application_1/style/app_colors.dart';
+import 'package:flutter_application_1/style/app_style.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
 
 class ArizaTexnikObyekt extends StatelessWidget {
   const ArizaTexnikObyekt({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
+    return const Scaffold(
+        body: SizedBox(
       width: double.infinity,
-      child: const Column(
+      child: Column(
         children: [
           MiniRedAppBar(),
           MiniRedTitle(title: 'Arizani Rasmiylashtirish'),
-          Expanded(child: aaaadawdsASdawd())
+          Expanded(child: ArizaStep())
         ],
       ),
     ));
   }
 }
 
-class aaaadawdsASdawd extends StatelessWidget {
-  const aaaadawdsASdawd({super.key});
+class ArizaStep extends StatelessWidget {
+  const ArizaStep({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +36,8 @@ class aaaadawdsASdawd extends StatelessWidget {
 }
 
 class MultiStepForm extends StatefulWidget {
+  const MultiStepForm({super.key});
+
   @override
   _MultiStepFormState createState() => _MultiStepFormState();
 }
@@ -42,7 +49,7 @@ class _MultiStepFormState extends State<MultiStepForm> {
   void _nextStep() {
     if (_currentStep < 3) {
       _pageController.nextPage(
-        duration: Duration(milliseconds: 300),
+        duration: const Duration(milliseconds: 300),
         curve: Curves.easeIn,
       );
       setState(() {
@@ -54,7 +61,7 @@ class _MultiStepFormState extends State<MultiStepForm> {
   void _previousStep() {
     if (_currentStep > 0) {
       _pageController.previousPage(
-        duration: Duration(milliseconds: 300),
+        duration: const Duration(milliseconds: 300),
         curve: Curves.easeIn,
       );
       setState(() {
@@ -71,7 +78,7 @@ class _MultiStepFormState extends State<MultiStepForm> {
           Expanded(
             child: PageView(
               controller: _pageController,
-              physics: NeverScrollableScrollPhysics(),
+              physics: const NeverScrollableScrollPhysics(),
               children: [
                 _buildStepPage1(),
                 _buildStepPage2(),
@@ -80,20 +87,22 @@ class _MultiStepFormState extends State<MultiStepForm> {
               ],
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ElevatedButton(
-                onPressed: _currentStep > 0 ? _previousStep : null,
-                child: Text('Oldingisi'),
-              ),
-              ElevatedButton(
-                onPressed: _currentStep < 3 ? _nextStep : null,
-                child: Text('Keyingisi'),
-              ),
-            ],
-          ),
-          SizedBox(
+          _currentStep < 3
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: _currentStep > 0 ? _previousStep : null,
+                      child: const Text('Oldingisi'),
+                    ),
+                    ElevatedButton(
+                      onPressed: _currentStep < 3 ? _nextStep : null,
+                      child: const Text('Keyingisi'),
+                    ),
+                  ],
+                )
+              : Container(),
+          const SizedBox(
             height: 50,
           )
         ],
@@ -105,26 +114,303 @@ class _MultiStepFormState extends State<MultiStepForm> {
     return Column(
       children: [
         Container(
-            padding: EdgeInsets.symmetric(horizontal: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 10),
             child: SvgPicture.asset(
               'assets/images/step1.svg',
               width: 50,
               height: 50,
             )),
+        Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Column(
+              children: [
+                Row(children: [
+                  Text(
+                    'Hududni tanlang',
+                    style: AppStyle.fontStyle
+                        .copyWith(fontWeight: FontWeight.bold),
+                  )
+                ]),
+              ],
+            ))
       ],
     );
   }
 
   Widget _buildStepPage2() {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         Container(
-            padding: EdgeInsets.symmetric(horizontal: 10),
             child: SvgPicture.asset(
-              'assets/images/step2.svg',
-              width: 50,
-              height: 50,
-            )),
+          'assets/images/step2.svg',
+          width: 50,
+          height: 50,
+        )),
+        SizedBox(
+          //margin: EdgeInsets.symmetric(horizontal: 50),
+          width: 219,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'O\'zbekiston Respublikasi Milliy Gvardiyasi ',
+                    style: AppStyle.fontStyle.copyWith(fontSize: 10),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Toshkent shaxar Qo\'riqlash boshqarmasi ',
+                    style: AppStyle.fontStyle.copyWith(fontSize: 10),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    ' boshlig\'i',
+                    style: AppStyle.fontStyle.copyWith(fontSize: 10),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Q.A.Shodibekovga ',
+                    style: AppStyle.fontStyle.copyWith(fontSize: 10),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '"Google" kompaniyasi',
+                    style: AppStyle.fontStyle
+                        .copyWith(fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Direktori: Raximov V.A',
+                    style: AppStyle.fontStyle.copyWith(fontSize: 10),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Telefon: (99)-786-25-51',
+                    style: AppStyle.fontStyle.copyWith(fontSize: 10),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: SizedBox(
+            width: double.infinity,
+            // height: 50,
+            child: AutoSizeText(
+              maxLines: 3,
+              '       Sizga, shuni ma’lum qilamizki Mirzo Ulug’bek tumani, A.Navoiy MFY Shodlik ko’chasi 17-uy joylashgan tashkilotimiz Qo’riqlovi va xavfsizligini Ta’minlash maqsadida ',
+              style: AppStyle.fontStyle.copyWith(fontSize: 10),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Perimetr(Harakat sezgi sensorlari)',
+              style: AppStyle.fontStyle
+                  .copyWith(fontSize: 12, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              width: 10,
+            ),
+            Text(
+              'Ish kunlari 09:00 / 17:00',
+              style: AppStyle.fontStyle.copyWith(fontSize: 10),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              width: 10,
+            ),
+            Text(
+              'Shanba va Yakshanba kunlari --:-- / --:--',
+              style: AppStyle.fontStyle.copyWith(fontSize: 10),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              width: 10,
+            ),
+            Text(
+              'Bayram kunlari --:-- / --:--',
+              style: AppStyle.fontStyle.copyWith(fontSize: 10),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Perimetr(Harakat sezgi sensorlari)',
+              style: AppStyle.fontStyle
+                  .copyWith(fontSize: 12, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              width: 10,
+            ),
+            Text(
+              'Ish kunlari 09:00 / 17:00',
+              style: AppStyle.fontStyle.copyWith(fontSize: 10),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              width: 10,
+            ),
+            Text(
+              'Shanba va Yakshanba kunlari --:-- / --:--',
+              style: AppStyle.fontStyle.copyWith(fontSize: 10),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              width: 10,
+            ),
+            Text(
+              'Bayram kunlari --:-- / --:--',
+              style: AppStyle.fontStyle.copyWith(fontSize: 10),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: SizedBox(
+            width: double.infinity,
+            // height: 50,
+            child: AutoSizeText(
+              maxLines: 3,
+              'TQM qo’riqloviga ulab  shartnoma tuzishda amaliy yordam berishingizni so’raymiz. Qo’riqlov uchun oylik to’lovlarni kafolatlaymiz',
+              style: AppStyle.fontStyle.copyWith(fontSize: 10),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 100,
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                Text(
+                  '«18» iyul 2024 yil ',
+                  style:
+                      AppStyle.fontStyle.copyWith(fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                const Row(
+                  children: [Text('"Google" kompaniyasi')],
+                ),
+                const Row(
+                  children: [Text('Direktori: Raximov V.A')],
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {}, child: const Text('Imzolash'))
+                  ],
+                ),
+              ],
+            )
+          ],
+        )
       ],
     );
   }
@@ -133,7 +419,7 @@ class _MultiStepFormState extends State<MultiStepForm> {
     return Column(
       children: [
         Container(
-            padding: EdgeInsets.symmetric(horizontal: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 10),
             child: SvgPicture.asset(
               'assets/images/step3.svg',
               width: 50,
@@ -144,39 +430,93 @@ class _MultiStepFormState extends State<MultiStepForm> {
   }
 
   Widget _buildStepPage4() {
-    return Container(
-      child: Center(
-        child: Column(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 20,
+        ),
+        SvgPicture.asset('assets/images/ariza_muvofaqiyat.svg'),
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'test',
-              style: TextStyle(fontSize: 24, color: Colors.white),
+              'Arizangiz',
+              style: AppStyle.fontStyle
+                  .copyWith(fontSize: 30, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'muvaffaqiyatli yuborildi',
+              style: AppStyle.fontStyle
+                  .copyWith(fontSize: 30, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Arizangiz ko’rib chiqilib',
+              style: AppStyle.fontStyle.copyWith(
+                fontSize: 15,
+              ),
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'siz bilan bog’lanamiz',
+              style: AppStyle.fontStyle.copyWith(
+                fontSize: 15,
+              ),
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class InnerPage extends StatelessWidget {
-  final String stepTitle;
-
-  InnerPage(this.stepTitle);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('$stepTitle - Внутренняя страница'),
-      ),
-      body: Center(
-        child: Text(
-          'Это внутренняя страница для $stepTitle',
-          style: TextStyle(fontSize: 24),
+        const SizedBox(
+          height: 20,
         ),
-      ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [],
+        ),
+        Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            width: double.infinity,
+            height: 70,
+            child: Expanded(
+              child: ElevatedButton(
+                onPressed: () {
+                  _currentStep = 0;
+                  pushScreenWithNavBar(context, BottomNavBar());
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Color(0xFF8B2D2D), // Цвет текста кнопки
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(20), // Радиус скругления углов
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20, vertical: 10), // Отступы внутри кнопки
+                ),
+                child: Text('Yopish',
+                    style: AppStyle.fontStyle.copyWith(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.lightHeaderColor)),
+              ),
+            ))
+      ],
     );
   }
 }
